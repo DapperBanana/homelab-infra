3fc3ffa25d889c4a2b4897fe6614754a83a30746
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-31 19:38:39 +0000

    Now working on cert-manager

---
+++ modules/cert-manager/helm.tf
@@ -0,0 +1,23 @@
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.14.4"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
+
+// === File: modules/cert-manager/values.yaml ===
+
+installCRDs: true
+
+# Additional config can go here if needed later
