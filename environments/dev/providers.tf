11a6928d2762b3809cd705a25fa125c9e9d9c228
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-01 22:16:03 +0000

    Updated with providers to see if that will fix the inheritence issue

---
+++ environments/dev/providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+    }
+  }
+}
