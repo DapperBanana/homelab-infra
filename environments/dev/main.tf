9ad8b8ecbd16cd325b92a652a66011f54d624cf8
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-01 13:16:46 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,14 @@
 terraform {
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.12.1"
-    }
     kubectl = {
       source = "gavinbunney/kubectl"
       version = "1.19.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.1"
+    }
+    
   }
 }
 
