715c9f2213549a88b13a9e42d50cc5b344433b90
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-01 22:03:51 +0000

    Updated main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 0.13"
+
   required_providers {
     kubectl = {
       source = "gavinbunney/kubectl"
@@ -19,7 +21,22 @@ provider "helm" {
 }
 
 provider "kubectl" {
+  apply_retry_count      = 15
+  host                   = var.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(var.eks_cluster_ca)
+  load_config_file       = false
+
   config_path = pathexpand("~/.kube/config")
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws-iam-authenticator"
+    args = [
+      "token",
+      "-i",
+      module.eks.cluster_id,
+    ]
+  }
 }
 
 module "traefik" {
