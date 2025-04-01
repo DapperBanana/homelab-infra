005e240af55f43ad02cdf1828d5e5c1138c2b841
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-01 22:17:48 +0000

    fixing main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,18 +1,4 @@
-terraform {
-  required_version = ">= 0.13"
 
-  required_providers {
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.12.1"
-    }
-    
-  }
-}
 
 provider "helm" {
   kubernetes {
