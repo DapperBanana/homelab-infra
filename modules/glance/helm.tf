c719130ceaba823cfce6654effe0136eb01a7647
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-02 04:23:32 +0000

    Claude suggestions for glance fix

--- modules/glance/helm.tf
+++ modules/glance/helm.tf
@@ -1,12 +1,35 @@
 resource "helm_release" "glance" {
   name             = "glance"
-  chart            = "glance"
-  repository       = "https://kubernetes-charts.storage.googleapis.com"
+  repository       = "https://nicolargo.github.io/helm-charts" # Updated repository
+  chart            = "glances"                                 # Chart name is "glances" not "glance"
+  version          = "1.0.5"                                  # Use a recent version
   namespace        = "glance"
   create_namespace = true
-  version          = "3.2.2" # or latest stable you prefer
-
+  
   values = [
-    file("${path.module}/values.yaml")
+    <<-EOT
+      glances:
+        container_name: glance
+        refresh_time: 5
+        theme: default
+        hide_top_bar: false
+        port: 61208
+      
+      ingress:
+        enabled: true
+        className: traefik
+        annotations:
+          traefik.ingress.kubernetes.io/router.entrypoints: "websecure"
+          traefik.ingress.kubernetes.io/router.tls: "true"
+        hosts:
+          - host: glance.austinlhoward.com
+            paths:
+              - path: /
+                pathType: Prefix
+        tls:
+          - hosts:
+              - glance.austinlhoward.com
+            secretName: glance-tls
+    EOT
   ]
 }
